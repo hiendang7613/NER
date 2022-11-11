fdcf5e54b5f1e68ce6b0841add9dbfb41fc843c2
Đặng Văn Hiển <hien-dv@fujinet.vn>
2022-11-11 10:36:59 +0700

    secondxx

---
+++ Config/Model/BiLSTMv1.py
@@ -0,0 +1,9 @@
+modelConfig = {
+    'backbone_type': 'BiLSTM',
+    'input_shape': (width, height, chanels)
+    'output_embedding_size': output_embedding_size  # backbone output
+    'embedding_type': 'phobert'
+                      'embedding_size': embedding_size  # backbone intput embedding
+'head_type': head_type,
+'num_classes': num_classes
+}'''
--- DataLoader/Dataloader.py
+++ DataLoader/Dataloader.py
@@ -1,6 +1,7 @@
 from DataReader.DataReader import DataReader
 from typing import List
 import numpy as np
+import tensorflow as tf
 
 class Dataloader(object):
     def __init__(self, dataReaders: List[DataReader]):
@@ -9,19 +10,31 @@ class Dataloader(object):
 
         self.doc_token_ids = []
         self.doc_labels = []
-        self.documents = []
         self.entity_type = []
         pass
 
-    def readData(self):
+    def getTokenIds(self):
+        return self.doc_token_ids
+
+    def getLabels(self):
+        return self.doc_labels
+
+    def getEntityType(self):
+        return self.entity_type
+
+    def getDataset(self):
+        return self.dataset
+
+    def load(self):
+        # read data
         for reader in self.dataReaders:
             reader.ParseAllDocs()
-            self.doc_token_ids.append(reader.getTokenIds())
+            self.doc_token_ids.extend(reader.getTokenIds())
             self.entity_type.append(reader.getEntityType())
 
         # merge array entity type
         self.entity_type = list(dict.fromkeys(self.entity_type))
-
+        # merge data of multiple reader data
         for reader in self.dataReaders:
             new_pos = [self.entity_type.index(e) for e in reader.getEntityType()]
             for doc_labels in reader.getLabels():
@@ -30,6 +43,12 @@ class Dataloader(object):
                     new_doc_labels[new_pos[k]] = doc_labels[k]
                 self.doc_labels.append(new_doc_labels)
 
-    def getDataset(self):
-        dataSet
-        return self.dataset
+        self.dataset = tf.data.Dataset.from_tensor_slices((self.doc_token_ids, self.doc_labels))
+        pass
+
+    def addTFRecord(self, tfrecord_path):
+        tfdataset = tf.data.TFRecordDataset(tfrecord_path)
+        self.dataset = self.dataset.concatenate(tfdataset)
+        pass
+
+
